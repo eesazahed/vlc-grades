98bc028eb5e238e16f3a0ffb28c1862e0ab956c8
Eesa Zahed <eszhd1@gmail.com>
2022-11-15 19:17:45 -0500

    make it lowercase

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 const Custom404: NextPage = () => {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>404 - Page Not Found</h1>
+      <h1 className={styles.title}>404 - Page not found</h1>
       <p className={styles.description}>
         <Link href="/">Go back</Link>
       </p>
